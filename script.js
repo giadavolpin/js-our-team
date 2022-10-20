878fbc9c94c2e65ab326d9f694503905cf10048e
giadavolpin <volpingiada@gmail.com>
2022-10-20 15:00:59 +0200

    image

---
+++ script.js
@@ -0,0 +1,30 @@
+const Team1 = {
+    nome: Wayne Barnett
+    role: Founder & Ceo
+    image: 
+}
+const Team2 = {
+    nome: Angela Caroll
+    role: Chief Editor
+    image:
+}
+const Team3 = {
+    nome: Walter Gordon
+    role: Office Manager
+    image:
+}
+const Team4 = {
+    nome: Angela Lopez
+    role: Social Media Manager
+    image:
+}
+const Team5 = {
+    nome: Scott Estrada
+    role: Developer
+    image:
+}
+const Team6 = {
+    nome: Barbara Ramos 
+    role: Graphic Designer
+    image:
+}
