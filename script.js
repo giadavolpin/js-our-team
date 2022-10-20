9d2f14e7649848a3125ee86698cdeac276d9725d
giadavolpin <volpingiada@gmail.com>
2022-10-20 15:24:34 +0200

    image

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
 const Team1 = {
-    nome: Wayne Barnett
-    role: Founder & Ceo
-    image: 
+    nome: 'Wayne Barnett'
+    role: 'Founder & Ceo'
+    const image = document.createElement("image1")
+    image1.src = "wa"
+    let image1 = document.getElementById('image1') 
 }
 const Team2 = {
-    nome: Angela Caroll
-    role: Chief Editor
+    nome: 'Angela Caroll'
+    role: 'Chief Editor'
     image:
 }
 const Team3 = {
-    nome: Walter Gordon
-    role: Office Manager
+    nome: 'Walter Gordon'
+    role: 'Office Manager'
     image:
 }
 const Team4 = {
-    nome: Angela Lopez
-    role: Social Media Manager
+    nome: 'Angela Lopez'
+    role: 'Social Media Manager'
     image:
 }
 const Team5 = {
-    nome: Scott Estrada
-    role: Developer
+    nome: 'Scott Estrada'
+    role: 'Developer'
     image:
 }
 const Team6 = {
-    nome: Barbara Ramos 
-    role: Graphic Designer
+    nome: 'Barbara Ramos' 
+    role: 'Graphic Designer'
     image:
 }
