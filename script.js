5085dbee5568e933afe335370a961ffc6fda36be
giadavolpin <volpingiada@gmail.com>
2022-11-08 20:29:48 +0100

    image

--- script.js
+++ script.js
@@ -1,33 +1,57 @@
-const Team1 = {
-    nome: 'Wayne Barnett';
-    role: 'Founder & Ceo';
-    let image1 = document.createElement("image1");
-    image1.src = "img\wayne-barnett-founder-ceo.jpg";
-    let image1 = document.getElementById('image1'); 
-    src.appendChild(image1);
-}
-const Team2 = {
-    nome: 'Angela Caroll'
-    role: 'Chief Editor'
-    image:
-}
-const Team3 = {
-    nome: 'Walter Gordon'
-    role: 'Office Manager'
-    image:
-}
-const Team4 = {
-    nome: 'Angela Lopez'
-    role: 'Social Media Manager'
-    image:
-}
-const Team5 = {
-    nome: 'Scott Estrada'
-    role: 'Developer'
-    image:
-}
-const Team6 = {
-    nome: 'Barbara Ramos' 
-    role: 'Graphic Designer'
-    image:
-}
+/* Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+MILESTONE 0:
+- Creare l’array di oggetti con le informazioni fornite.
+MILESTONE 1:
+Stampare su console le informazioni di nome, ruolo e la stringa della foto
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+BONUS 1:
+Trasformare la stringa foto in una immagine effettiva
+BONUS 2:
+Organizzare i singoli membri in card/schede e stilarli a vostro gusto :faccia_leggermente_sorridente:
+BONUS 3:
+Aggiungere attraverso un form un membro al team */
+
+let Teams = {
+    Team1: [{
+        nome: 'Wayne Barnett',
+        role: 'Founder & Ceo',
+        immagine1:
+            let immagine1 = document.getElementById('image1'), //prendo ID
+            let immagine1 = document.createElement("image1"),  // creo l'elemento 
+
+
+            
+         "img\wayne-barnett-founder-ceo.jpg"
+       
+        src.appendChild(image1), 
+    }, 
+   /*  Team2 = {
+        nome: 'Angela Caroll',
+        role: 'Chief Editor',
+        image:
+    },
+    Team3 = {
+        nome: 'Walter Gordon',
+        role: 'Office Manager',
+        image:
+    },
+    Team4 = {
+        nome: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image:
+    },
+    Team5 = {
+        nome: 'Scott Estrada',
+        role: 'Developer',
+        image:
+    },
+    Team6 = {
+        nome: 'Barbara Ramos', 
+        role: 'Graphic Designer',
+        image:*/
+    ] 
+ };
+console.log(Teams); 
+
